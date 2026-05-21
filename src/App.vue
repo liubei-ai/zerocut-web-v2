a6f1c2b67d8983dcc1848d5fa90cd9d15e658aad
lidongjies <lidongjies@gmail.com>
2026-05-21 21:58:27 +0800

    Revert "feat(membership): auto-open modal when credits drop below 50"
    This reverts commit 87450bb79f38131c75cd296fcd2e33179cabdf63.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup lang="ts">
-import { watch } from 'vue';
 import { Toaster, ToastProvider } from '@/components/ui/toast';
 import MembershipModal from '@/components/membership/MembershipModal.vue';
 import { useMembershipModalStore } from '@/stores/membershipModalStore';
-import { useCreditsStore } from '@/stores/creditsStore';
 
 const membershipModalStore = useMembershipModalStore();
-const creditsStore = useCreditsStore();
-
-watch(
-  () => creditsStore.creditsBalance,
-  balance => {
-    if (typeof balance === 'number') {
-      membershipModalStore.tryAutoOpenForLowCredits(balance);
-    }
-  },
-);
 </script>
 
 <template>
